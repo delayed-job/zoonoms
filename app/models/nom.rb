c1d04bc766f24fdf0c55861a5ef9c2795c1a1dad
Mike Skalnik and Will Farrington <delayedjob+skalnik+wfarr@gmail.com>
2010-03-12 17:12:10 -0500

    Add animal and user methods to nom

--- app/models/nom.rb
+++ app/models/nom.rb
@@ -1,4 +1,12 @@
 class Nom < ActiveRecord::Base
   has_one :animal
   belongs_to :user
+
+  def animal
+    Animal.find(animal_id).name
+  end
+
+  def user
+    User.find(user_id).email
+  end
 end
