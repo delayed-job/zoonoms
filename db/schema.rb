826018d38cacefcc6ec5a7831f41d6e3242464e5
Mike Skalnik <mike.skalnik@gmail.com>
2010-03-12 14:13:24 -0500

    Animals migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100312185459) do
+ActiveRecord::Schema.define(:version => 20100312185734) do
+
+  create_table "animals", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                              :null => false
