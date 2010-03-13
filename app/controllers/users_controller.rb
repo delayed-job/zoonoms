001e62ed78dd37baad6a349cebc3243be4ffee88
Mike Skalnik <mike.skalnik@gmail.com>
2010-03-12 22:43:01 -0500

    Updated user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = params[:id] ? User.find(params[:id]) : User.find_by_username(params[:username])
-    
+    @noms = @user.noms.order('created_at DESC').limit(10)
+
     respond_to do |format|
       format.html
       format.xml { render :xml => @user }
